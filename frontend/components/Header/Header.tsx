3746821e9ef1813aa73b10e3756dfa904966c041
ray03j <eihouse.potato@outlook.jp>
2024-10-19 11:06:12 +0900

    [feat] header color

--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { allowedCategories } from '@/app/constants';
-import { theme } from '@/app/theme';
 
 export const Header = () => {
   return (
     <Box sx={{ display: 'flex'}}>
-      <AppBar component="nav" sx={{ bgcolor: theme.palette.primary.main}}>
-        <Toolbar >
+      <AppBar component="nav" sx={{bgcolor: "primary.main"}}>
+        <Toolbar>
           <Typography
             variant="h5"
             component="div"
